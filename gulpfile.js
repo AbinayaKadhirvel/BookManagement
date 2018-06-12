7ddd59b9bc33c8c4492da4ba73c0a62c511efffe
Karpagambigai <36296018+Karpagambigai@users.noreply.github.com>
2018-06-12 11:18:49 +0530

    Changes to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -33,7 +33,7 @@ gulp.task('default', () => {
 gulp.task('test', () => {
   env({ vars: { ENV: 'Test' } });
   gulp.src('./src/tests/*.js', { read: false })
-    .pipe(gulpMocha({ reporter: 'nyan' }));
+    .pipe(gulpMocha({ reporter: 'nyan', timeout: 5000 }));
 });
 
 
